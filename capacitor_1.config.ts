0e45ea438bd7c2bd459e7b5c4e6b290f5e81126d
utthang1606 <hang.uy@mobi.ch>
2023-08-29 14:46:40 +0700

    remove redundant code

--- capacitor_1.config.ts
+++
@@ -1,9 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'tan.test.ionic.starter.test',
-  appName: 'dynamicComponentApp',
-  webDir: 'www'
-};
-
-export default config;
