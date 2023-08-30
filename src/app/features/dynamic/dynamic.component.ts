8f75a66663b7bad8eef63af2a952dd925bfdbfc7
Tai Tran <tai.tran@mobi.ch>
2023-08-30 10:10:38 +0700

    commit old code

--- src/app/features/dynamic/dynamic.component.ts
+++ src/app/features/dynamic/dynamic.component.ts
@@ -3,13 +3,20 @@ import {
   Input,
   ViewChild,
   ViewContainerRef,
-  ComponentRef, Injector, forwardRef
+  ComponentRef,
+  Injector,
+  forwardRef,
+  Renderer2,
 } from '@angular/core';
-import {Child1Component} from "./components/child1/child1.component";
-import {ItemInputComponent} from '../../shared/component/item-input-component/item-input.component';
-import {FormBuilder, FormControl, FormGroup, NG_VALUE_ACCESSOR} from "@angular/forms";
-import {IonInput} from "@ionic/angular";
-
+import { Child1Component } from './components/child1/child1.component';
+import { ItemInputComponent } from '../../shared/component/item-input-component/item-input.component';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-parent',
@@ -34,38 +41,37 @@ export class DynamicComponent {
 
   //Get tag child component will be placed
   // @ts-ignore
-  @ViewChild('target', {read: ViewContainerRef}) target: ViewContainerRef;
+  @ViewChild('target', { read: ViewContainerRef }) target: ViewContainerRef;
   // @ts-ignore
   private componentRef: ComponentRef<any>;
-
 
   //Child components
   private children = [
     {
       component: Child1Component,
       value: 'child 1',
-      formControlName: ''
+      formControlName: '',
     },
     {
       component: IonInput,
-      label: 'Ion Input Label',
+      label: 'Tai Dep Trai',
       formControlName: 'ionInput',
-      value: 'child 2'
+      value: 'child 2',
     },
     {
       component: ItemInputComponent,
       label: 'Item Input Label',
       value: 'input component',
-      formControlName: 'itemInput'
-    }
+      formControlName: 'itemInput',
+    },
   ];
 
   constructor(
     private viewContainerRef: ViewContainerRef,
     public fb: FormBuilder,
-    public injector: Injector
-  ) {
-  }
+    public injector: Injector,
+    private renderer: Renderer2
+  ) {}
 
   //Pass through value to child component
   renderComponent() {
@@ -73,7 +79,6 @@ export class DynamicComponent {
   }
 
   public async ngOnInit(): Promise<void> {
-
     const formGroupFields = this.getFormControlsFields();
     this.testUser = new FormControl('mmm');
     formGroupFields['testUser'] = new FormControl('');
@@ -83,12 +88,14 @@ export class DynamicComponent {
 
   getFormControlsFields() {
     const formGroupFields = {};
-    this.children.map(item => item.formControlName).forEach((field) => {
-      console.log('field: ', field);
-      if (field) {
-        formGroupFields[field] = new FormControl('aaaa');
-      }
-    });
+    this.children
+      .map((item) => item.formControlName)
+      .forEach((field) => {
+        console.log('field: ', field);
+        if (field) {
+          formGroupFields[field] = new FormControl('aaaa');
+        }
+      });
     return formGroupFields;
   }
 
@@ -98,26 +105,37 @@ export class DynamicComponent {
     // let childComponent = this.children[this.child || 'child1'];
     //Resolve child component
     this.children.forEach((item) => {
-
       // const ngControl = this.injector.get(NgControl);
       // @ts-ignore
       this.componentRef = this.target.createComponent(item.component);
       if (this.componentRef) {
         // this.componentRef.instance.value = this.testUserForm.controls[item.formControlName].value;
         this.componentRef.instance.label = item.label;
-        console.log('form control: ', this.testUserForm.controls[item.formControlName]);
-        this.componentRef.instance.formControl = this.testUser;
-        this.componentRef.instance.formControlName = this.testUserForm.controls[item.formControlName];
+        console.log(
+          'form control: ',
+          this.testUserForm.controls[item.formControlName]
+        );
+        // this.componentRef.instance.formControl = this.testUser;
+        this.renderer.setProperty(
+          this.componentRef.location.nativeElement,
+          'formControlName',
+          item.formControlName
+        );
+        this.componentRef.instance.control =
+          this.testUserForm.controls[item.formControlName];
       }
     });
   }
 
   public trackFormChangeEvent(): void {
     this.testUserForm.valueChanges.subscribe(async (formData) => {
-      console.log('formData data: ', formData)
+      console.log('formData data: ', formData);
     });
   }
 
+  public getFormData(): void {
+    console.error(this.testUserForm.value);
+  }
   //Pass through value to child component when value changes
   ngOnChanges(changes: Object) {
     this.renderComponent();
--- src/app/features/dynamic/dynamic.module.ts
+++ src/app/features/dynamic/dynamic.module.ts
@@ -1,20 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { DynamicRoutingModule } from './dynamic-routing.module';
-import {Child2Component} from "./components/child2/child2.component";
-import {Child1Component} from "./components/child1/child1.component";
-import {DynamicComponent} from "./dynamic.component";
-import {SharedModule} from "../../shared/shared.module";
+import { Child2Component } from './components/child2/child2.component';
+import { Child1Component } from './components/child1/child1.component';
+import { DynamicComponent } from './dynamic.component';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-    declarations: [
-      Child2Component,
-      Child1Component,
-      DynamicComponent
-    ],
-    providers: [],
-    imports: [IonicModule, CommonModule, DynamicRoutingModule, SharedModule]
+  declarations: [Child2Component, Child1Component, DynamicComponent],
+  providers: [],
+  imports: [
+    IonicModule,
+    CommonModule,
+    DynamicRoutingModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class DynamicModule {}
